5ab42db3f0ab612d2701a2b1fa6b9b59d88591ba
bright.k <bright.k@toss.im>
2023-08-03 12:04:03 +0900

    initialized project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "ddd-study-board"
+
+include(":server")
+include(":user-module")
+include(
+    ":user-module:user-domain",
+)
+include(":board-module")
+include(
+    ":board-module:board-domain",
+)
+include(":follow-module")
+include(
+    ":follow-module:follow-domain",
+)
+include(":notification-module")
+include(
+    ":notification-module:notification-domain",
+)
